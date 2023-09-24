9f14c431829c1b0f2b5dd06f7d8224b217822933
Ahmad Fikri Nurrahman <ahmadfikri.n@gmail.com>
2023-09-24 04:37:48 +0000

    #25 Restructure Root Module

--- main.tf
+++ main.tf
@@ -1,38 +1,3 @@
-terraform {
-  # backend "remote" {
-  #   hostname = "app.terraform.io"
-  #   organization = "ahmadfikrin"
-
-  #   workspaces {
-  #     name = "terra-house-1"
-  #   }
-  # }
-  cloud {
-    organization = "ahmadfikrin"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   lower = true
   upper = false
@@ -42,8 +7,8 @@ resource "random_string" "bucket_name" {
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid # using terraform cloud workspace variable - type environment variable
+  }
 }
