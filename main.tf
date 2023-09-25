194c65e3c288cfd18d41e0b0b777cc01df0ddca2
ahmadfikrin <ahmadfikri.n@gmail.com>
2023-09-25 22:05:39 +0700

    #29 refactor s3 bucket to Terrahouse Module (#30)

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+terraform {
 # resource "random_string" "bucket_name" {
 #   lower = true
 #   upper = false
@@ -5,11 +6,18 @@
 #   special = false
 # }
 
-resource "aws_s3_bucket" "website_bucket" {
-  # bucket = random_string.bucket_name.result
-  bucket = var.bucket_name
+# resource "aws_s3_bucket" "website_bucket" {
+#   # bucket = random_string.bucket_name.result
+#   bucket = var.bucket_name
 
-  tags = {
-    UserUuid = var.user_uuid # using terraform cloud workspace variable - type environment variable
-  }
+#   tags = {
+#     UserUuid = var.user_uuid # using terraform cloud workspace variable - type environment variable
+#   }
+# }
+}
+
+module "terrahouse_aws" {
+    source = "./modules/terrahouse_aws"
+    user_uuid = var.user_uuid
+    bucket_name = var.bucket_name
 }
--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -19,4 +19,3 @@ variable "bucket_name" {
     error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
   }
 }
-
