fc2caafbde3d7db84c7c47ea89ac28a102f34f7f
Ahmad Fikri Nurrahman <ahmadfikri.n@gmail.com>
2023-09-25 14:25:18 +0000

    #27 Import and configuration drift

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
-resource "random_string" "bucket_name" {
-  lower = true
-  upper = false
-  length  = 32
-  special = false
-}
+# resource "random_string" "bucket_name" {
+#   lower = true
+#   upper = false
+#   length  = 32
+#   special = false
+# }
 
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
+resource "aws_s3_bucket" "website_bucket" {
+  # bucket = random_string.bucket_name.result
+  bucket = var.bucket_name
 
   tags = {
     UserUuid = var.user_uuid # using terraform cloud workspace variable - type environment variable
