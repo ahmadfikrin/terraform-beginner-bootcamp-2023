7becd75575fb35b94813e0ecf2f7ac498a751aac
Ahmad Fikri Nurrahman <ahmadfikri.n@gmail.com>
2023-09-28 07:51:13 +0000

    #35 content version

--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -4,6 +4,7 @@ resource "aws_cloudfront_origin_access_control" "default" {
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
+
 }
 
 locals {
