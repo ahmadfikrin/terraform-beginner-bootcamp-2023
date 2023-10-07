ebd2cecbd93a330b131f120347319cd10f34ab16
ahmadfikrin <ahmadfikri.n@gmail.com>
2023-10-07 17:12:53 +0700

    #43 terratown provider skel (#45)
    * #43 Terratown Provider
    
    * #43 terratown provider

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket name for our static website hosting"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "website_endpoint" {
-  description = "S3 Static Website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "website_endpoint" {
+#   description = "S3 Static Website hosting endpoint"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {
-  description = "The CloudFront Distribution Domain Name"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "The CloudFront Distribution Domain Name"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
